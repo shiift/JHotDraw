fcb2e1a899dae4c332c7db7c3ca3113f2761d563
wsd10001 <william.dickson@uconn.edu>
2014-03-25 18:51:53 -0400

    Added HtmlSelectionTool and HtmlToolBarButtonFactory
    (Removed FigureManager and EditorApplicationModel)

---
+++ CSE2102_Group39/src/htmleditor/HtmlSelectionTool.java
@@ -0,0 +1,26 @@
+package htmleditor;
+
+import java.awt.event.MouseEvent;
+import java.util.Collection;
+
+import javax.swing.Action;
+
+import org.jhotdraw.draw.DelegationSelectionTool;
+
+public class HtmlSelectionTool extends DelegationSelectionTool {
+	
+	public HtmlSelectionTool(){
+		super();
+	}
+
+	public HtmlSelectionTool(Collection<Action> drawingActions,
+			Collection<Action> selectionActions) {
+		super(drawingActions, selectionActions);
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent evt){
+		super.mouseReleased(evt);
+		System.out.println("Mouse Released HTML");
+	}
+}
---
+++ CSE2102_Group39/src/htmleditor/HtmlToolBarButtonFactory.java
@@ -0,0 +1,77 @@
+package htmleditor;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import javax.swing.Action;
+import javax.swing.ButtonGroup;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.Tool;
+import org.jhotdraw.draw.ToolEvent;
+import org.jhotdraw.draw.ToolListener;
+import org.jhotdraw.draw.action.ToolBarButtonFactory;
+import org.jhotdraw.util.ResourceBundleUtil;
+
+public class HtmlToolBarButtonFactory extends ToolBarButtonFactory{
+	
+	protected HtmlToolBarButtonFactory(){
+	}
+	
+    public static void addSelectionToolTo(JToolBar tb, final DrawingEditor editor) {
+        addSelectionToolTo(tb, editor, createDrawingActions(editor), createSelectionActions(editor));
+    }
+    public static void addSelectionToolTo(JToolBar tb, final DrawingEditor editor,
+            Collection<Action> drawingActions, Collection<Action> selectionActions) {
+        ResourceBundleUtil labels = ResourceBundleUtil.getLAFBundle("org.jhotdraw.draw.Labels");
+        
+        JToggleButton t;
+        Tool tool;
+        HashMap<String,Object> attributes;
+        
+        ButtonGroup group;
+        if (tb.getClientProperty("toolButtonGroup") instanceof ButtonGroup) {
+            group = (ButtonGroup) tb.getClientProperty("toolButtonGroup");
+        } else {
+            group = new ButtonGroup();
+            tb.putClientProperty("toolButtonGroup", group);
+        }
+        
+        // Selection tool
+        Tool selectionTool = new HtmlSelectionTool(
+                drawingActions, selectionActions
+                );
+        editor.setTool(selectionTool);
+        t = new JToggleButton();
+        final JToggleButton defaultToolButton = t;
+        
+        ToolListener toolHandler;
+        if (tb.getClientProperty("toolHandler") instanceof ToolListener) {
+            toolHandler = (ToolListener) tb.getClientProperty("toolHandler");
+        } else {
+            toolHandler = new ToolListener() {
+                public void toolStarted(ToolEvent event) {
+                }
+                
+                public void toolDone(ToolEvent event) {
+                    defaultToolButton.setSelected(true);
+                }
+                
+                public void areaInvalidated(ToolEvent e) {
+                }
+            };
+            tb.putClientProperty("toolHandler", toolHandler);
+        }
+        
+        labels.configureToolBarButton(t, "selectionTool");
+        t.setSelected(true);
+        t.addItemListener(
+                new ToolButtonListener(selectionTool, editor)
+                );
+        t.setFocusable(false);
+        group.add(t);
+        tb.add(t);
+    }
+}
