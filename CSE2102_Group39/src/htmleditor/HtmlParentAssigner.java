2a8fd5b2a1ab0215ff95fd97a4fe08024adb7e56
wil11001 <william.lukaszewski@uconn.edu>
2014-03-26 20:35:59 -0400

    Fixed HtmlParentParser.

--- CSE2102_Group39/src/htmleditor/HtmlParentAssigner.java
+++ CSE2102_Group39/src/htmleditor/HtmlParentAssigner.java
@@ -22,19 +22,18 @@ public class HtmlParentAssigner implements ActionListener{
 				LinkedList<HtmlFigure> possibleParents = new LinkedList<HtmlFigure>();
 				//Possible Parents are added to newly created linklist for the current figure.
 				for(int j = 0; j<Global.figureList.size(); j++){
-					HtmlFigure curPossibleParent = Global.figureList.pop();
+					HtmlFigure curPossibleParent = Global.figureList.get(j);
 					Rectangle2D.Double posRec = curPossibleParent.rectangle;
 					if(posRec.x<=curRec.x && posRec.y<=curRec.y && posRec.x + posRec.width>=curRec.x + curRec.width && posRec.y + posRec.height>=curRec.y + curRec.height){
 						possibleParents.add(curPossibleParent);
 					}
-					Global.figureList.add(curPossibleParent);
 				}
 				//Possible Parents are looped to see the smallest of the parent and assign it as such.
-				HtmlFigure _parent = possibleParents.pop();
-				if(possibleParents.isEmpty()==false){
-					for(int j = 0; j<possibleParents.size(); j++){
+				HtmlFigure _parent = possibleParents.get(0);
+				if(possibleParents.size()>1){
+					for(int j = 1; j<possibleParents.size(); j++){
 						Rectangle2D.Double curParRec = _parent.rectangle;
-						HtmlFigure posPar = possibleParents.pop();
+						HtmlFigure posPar = possibleParents.get(j);
 						Rectangle2D.Double posParRec = posPar.rectangle;
 						if(posParRec.x>=curParRec.x && posParRec.y>=curParRec.x && posParRec.x + posParRec.width <= curParRec.x + curParRec.width && posParRec.y + posParRec.height <= curParRec.y + curParRec.height){
 							_parent = posPar;
@@ -46,6 +45,5 @@ public class HtmlParentAssigner implements ActionListener{
 			}
 			Global.figureList.add(curFig);
 		}
-	}
-
+	}	
 }
