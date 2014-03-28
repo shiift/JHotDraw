15f831c9435044f991ba7af4e3b6c34afc19458b
wsd10001 <william.dickson@uconn.edu>
2014-03-27 20:23:47 -0400

    Added labels for Div tag

--- CSE2102_Group39/src/htmleditor/DrawApplicationModel.java
+++ CSE2102_Group39/src/htmleditor/DrawApplicationModel.java
@@ -103,6 +103,7 @@ public class DrawApplicationModel extends DefaultApplicationModel {
     public void addDefaultCreationButtonsTo(JToolBar tb, final DrawingEditor editor,
             Collection<Action> drawingActions, Collection<Action> selectionActions) {
         ResourceBundleUtil labels = ResourceBundleUtil.getLAFBundle("org.jhotdraw.draw.Labels");
+        ResourceBundleUtil htmlLabels = ResourceBundleUtil.getLAFBundle("htmleditor.Labels");
         
         HtmlToolBarButtonFactory.addSelectionToolTo(tb, editor, drawingActions, selectionActions);
         tb.addSeparator();
@@ -113,7 +114,7 @@ public class DrawApplicationModel extends DefaultApplicationModel {
         ConnectionFigure lc;
         
         HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new HtmlFigure()), "createRectangle", labels);
-        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new DivFigure()), "createTriangle", labels);
+        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new DivFigure()), "createDiv", htmlLabels);
 //        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new RoundRectangleFigure()), "createRoundRectangle", labels);
 //        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new EllipseFigure()), "createEllipse", labels);
 //        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new DiamondFigure()), "createDiamond", labels);
