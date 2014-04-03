d65b910d11b4bc208934f474f08e938a88624532
wsd100001 <william.dickson@uconn.edu>
2014-04-03 01:28:20 -0400

    Added a few comments

--- CSE2102_Group39/src/htmleditor/Global.java
+++ CSE2102_Group39/src/htmleditor/Global.java
@@ -3,6 +3,7 @@ package htmleditor;
 import java.util.LinkedList;
 
 public class Global {
+	// Global variables that may be removed in the future
 	public static HtmlFigure topParent;
 	public static LinkedList<HtmlFigure> figureList;
 }
