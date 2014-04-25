f27fb1d1c583f58ef83b8a9f82f67b39bd046701
Brianna <brianna.mathiowetz@uconn.edu>
2014-04-24 23:25:30 -0400

    Comments

---
+++ CSE2102_Group39/src/htmleditor/DefaultHtmlDrawing.java
@@ -0,0 +1,42 @@
+package htmleditor;
+
+import java.awt.Color;
+
+import org.jhotdraw.draw.AttributeKeys;
+import org.jhotdraw.draw.DefaultDrawing;
+
+
+public class DefaultHtmlDrawing extends DefaultDrawing {
+	private DrawProject project;
+	private TopParentHtmlFigure topParent;
+
+	public DefaultHtmlDrawing() {
+    }
+
+	public void setProject(DrawProject project){
+		this.project = project;  
+	}
+
+	public DrawProject getProject(){
+		return this.project;
+	}
+
+	public TopParentHtmlFigure createTopParent(DefaultHtmlDrawingView view){
+		topParent = new TopParentHtmlFigure(100, 100, 600, 800);
+        topParent.addHtmlAttribute(topParent, "style", "background:#FFF;width:800px;margin:0 auto;border:1px solid black;min-height:800px;", false);
+        topParent.isTopParent = true;
+        topParent.setName("Base Page");
+		topParent.setAttribute(AttributeKeys.FILL_COLOR, Color.LIGHT_GRAY);
+		topParent.setParent(null);
+		view.getDrawing().add(topParent);
+		return topParent;
+	}
+
+	public TopParentHtmlFigure getTopParent(){
+		return topParent;
+	}
+
+	public void setTopParent(TopParentHtmlFigure tp){
+		topParent = tp;
+	}
+}
---
+++ CSE2102_Group39/src/htmleditor/DrawApplicationModel.java
@@ -0,0 +1,108 @@
+/*
+ * @(#)DrawApplicationModel.java  1.0  June 10, 2006
+ *
+ * Copyright (c) 1996-2006 by the original authors of JHotDraw
+ * and all its contributors ("JHotDraw.org")
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * JHotDraw.org ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with
+ * JHotDraw.org.
+ */
+
+package htmleditor;
+
+import org.jhotdraw.util.*;
+
+import htmleditor.HtmlFigure;
+
+import java.util.*;
+
+import javax.swing.*;
+
+import org.jhotdraw.app.*;
+import org.jhotdraw.draw.*;
+import org.jhotdraw.draw.action.*;
+
+import static org.jhotdraw.draw.AttributeKeys.*;
+/**
+ * DrawApplicationModel.
+ * 
+ * 
+ * 
+ * @author Werner Randelshofer.
+ * @version 1.0 June 10, 2006 Created.
+ */
+public class DrawApplicationModel extends DefaultApplicationModel {
+    /**
+     * This editor is shared by all projects.
+     */
+    private DefaultDrawingEditor sharedEditor;
+    
+    /** Creates a new instance. */
+    public DrawApplicationModel() {
+    }
+    
+    public DefaultDrawingEditor getSharedEditor() {
+        if (sharedEditor == null) {
+            sharedEditor = new DefaultDrawingEditor();
+        }
+        return sharedEditor;
+    }
+    
+    public void initProject(Application a, Project p) {
+        if (a.isSharingToolsAmongProjects()) {
+            ((DrawProject) p).setEditor(getSharedEditor());
+        }
+    }
+    /**
+     * Creates toolbars for the application.
+     * This class always returns an empty list. Subclasses may return other
+     * values.
+     */
+    public List<JToolBar> createToolBars(Application a, Project pr) {
+        ResourceBundleUtil labels = ResourceBundleUtil.getLAFBundle("org.jhotdraw.draw.Labels");
+        DrawProject p = (DrawProject) pr;
+        
+        DrawingEditor editor;
+        if (p == null) {
+            editor = getSharedEditor();
+        } else {
+            editor = p.getEditor();
+        }
+        
+        LinkedList<JToolBar> list = new LinkedList<JToolBar>();
+        JToolBar tb;
+        tb = new JToolBar();
+        addCreationButtonsTo(tb, editor);
+        tb.setName(labels.getString("drawToolBarTitle"));
+        list.add(tb);
+        
+        tb = new JToolBar();
+        HtmlToolBarButtonFactory.addExportButtonsTo(tb, editor);
+        tb.setName("Export ToolBar");
+        list.add(tb);
+        return list;
+    }
+    private void addCreationButtonsTo(JToolBar tb, DrawingEditor editor) {
+        addDefaultCreationButtonsTo(tb, editor, 
+                HtmlToolBarButtonFactory.createDrawingActions(editor), 
+                HtmlToolBarButtonFactory.createSelectionActions(editor)
+                );
+    }
+    public void addDefaultCreationButtonsTo(JToolBar tb, final DrawingEditor editor,
+            Collection<Action> drawingActions, Collection<Action> selectionActions) {
+        ResourceBundleUtil labels = ResourceBundleUtil.getLAFBundle("org.jhotdraw.draw.Labels");
+        ResourceBundleUtil htmlLabels = ResourceBundleUtil.getLAFBundle("htmleditor.Labels");
+        
+        HtmlToolBarButtonFactory.addSelectionToolTo(tb, editor, drawingActions, selectionActions);
+        tb.addSeparator();
+        
+        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new DivFigure()), "createDiv", htmlLabels);
+        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new ImgFigure()), "createImg", htmlLabels);
+        HtmlToolBarButtonFactory.addToolTo(tb, editor, new CreationTool(new HRFigure()), "createHR", htmlLabels);
+        HtmlToolBarButtonFactory.addToolTo(tb, editor, new TextAreaTool(new ParagraphFigure()), "createTextArea", labels);
+    }    
+}
