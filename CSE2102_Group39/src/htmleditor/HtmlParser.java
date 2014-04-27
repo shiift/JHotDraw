135aa2ba29d51bff9a2fb42198f1a19d528415c3
Brianna <brianna.mathiowetz@uconn.edu>
2014-04-27 18:22:41 -0400

    Fixes

---
+++ CSE2102_Group39/src/htmleditor/HtmlParser.java
@@ -0,0 +1,58 @@
+package htmleditor;
+
+import htmleditor.figures.AbstractTextFigure;
+import htmleditor.figures.HtmlFigure;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+public class HtmlParser {
+
+	public static PrintWriter writer;
+
+	// parses main html parser
+	static public void createFile(HtmlFigure hf, File file) throws FileNotFoundException, UnsupportedEncodingException{
+		writer = new PrintWriter(file.getAbsolutePath(), "UTF-8");
+
+		writer.println("<html>\n<body>\n");
+		parseHtml(hf, 1);
+		writer.println("</body>\n</html>\n");
+		writer.close();
+	}
+
+	// parses html recursively
+	static public void parseHtml(HtmlFigure hf, int depth){
+		print("<" + hf.getTag(), depth);
+		int loc = 0;
+		for(String name : hf.getAttributeList().keySet()){
+				if(hf.getAttributeList().get(name).getValue() != ""){
+					writer.print(" " + name + "=\"" + hf.getAttributeList().get(name).getValue() + "\" ");
+				}
+
+			loc++;
+		}
+		writer.print(" style=\"" + hf.getStyleString() + " position:absolute;\" ");
+		writer.println(">");
+		if(hf instanceof AbstractTextFigure){
+			writer.print(((AbstractTextFigure) hf).getParsedText());
+		}
+		for(int i = 0; i < hf.getObjectList().size(); i++){
+			parseHtml(hf.getObjectList().get(i), depth + 1);
+		}
+		print("</" + hf.getTag() + ">\n", depth);
+	}
+
+	private static String parseLoc(HtmlFigure hf, int loc) {
+		String rString = " position: absolute; left: " + hf.rectangle.x + "; top: " + hf.rectangle.y + ";";
+		return rString;
+	}
+
+	static public void print(String line, int depth){
+		for(int i = 0; i < depth; i++){
+			writer.print("\t");
+		}
+		writer.print(line);
+	}
+}
---
+++ CSE2102_Group39/src/htmleditor/figures/AFigure.java
@@ -0,0 +1,45 @@
+package htmleditor.figures;
+
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.util.Map;
+
+import org.jhotdraw.draw.AttributeKeys;
+import org.jhotdraw.draw.Figure;
+
+public class AFigure extends AbstractTextFigure {
+
+	private boolean control = false;
+
+    /** Creates a new instance. */
+    public AFigure() {
+        this("Link Text");
+    }
+    
+    public AFigure(String text) {
+        setText(text);
+        setTag("a");
+		setName("Anchor");
+    }
+    
+    public AFigure clone() {
+    	super.clone();
+    	AFigure that = (AFigure) super.clone();
+    	that.setName("Anchor");
+    	that.setTag("a");
+    	
+    	this.addHtmlAttribute(that, "href", "http://www.web.com");
+        return that;
+    }
+    
+    public void basicSetBounds(Point2D.Double anchor, Point2D.Double lead) 
+	{
+		super.basicSetBounds(anchor, lead);
+		if(control==false){
+			this.setAttribute(AttributeKeys.FILL_COLOR, Color.ORANGE);
+			control = true;
+		}
+	}
+
+}
