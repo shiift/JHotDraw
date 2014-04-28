21bf3158cad69459b8adbe460c608d636acc98fb
Brianna <brianna.mathiowetz@uconn.edu>
2014-04-27 22:17:08 -0400

    nothing...

--- CSE2102_Group39/src/htmleditor/AttributePanel.java
+++ CSE2102_Group39/src/htmleditor/AttributePanel.java
@@ -20,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
